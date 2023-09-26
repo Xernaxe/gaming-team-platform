8da92dd784311d00809abb8308841b0c79a3efcd
Xernaxe <93713092+Xernaxe@users.noreply.github.com>
2023-09-26 11:36:44 +0300

    Init

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import './globals.css'
 import type { Metadata } from 'next'
+import 'tailwindcss/tailwind.css';
 import { Inter } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
