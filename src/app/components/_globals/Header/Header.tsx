46477196b93475b707e6f9fd86ad7d4ad6f27c4d
Mihai-Alexandru Vistran <93713092+Xernaxe@users.noreply.github.com>
2023-10-11 17:07:30 +0300

    folder structure for the rest of the navbar pages

--- src/app/components/_globals/Header/Header.tsx
+++ src/app/components/_globals/Header/Header.tsx
@@ -5,12 +5,12 @@ import { NavbarDesktop } from './Navbar/NavbarDesktop';
 
 export const Header = () => {
 	const navLinks: INavLinks[] = [
-		{ name: 'Tournaments', href: '#' },
-		{ name: 'Our Team', href: '#' },
-		{ name: 'Blog', href: '#' },
-		{ name: 'About Us', href: '#' },
-		{ name: 'Shop', href: '#' },
-		{ name: 'Contact Us', href: '#' },
+		{ name: 'Tournaments', href: '/tournaments' },
+		{ name: 'Our Team', href: '/team' },
+		{ name: 'Blog', href: '/blog' },
+		{ name: 'About Us', href: '/about' },
+		{ name: 'Shop', href: '/shop' },
+		{ name: 'Contact Us', href: '/contact' },
 	];
 
 	return (
