d02f6d676988a5c373a5a1c89cc72c9b62d402ea
Mihai-Alexandru Vistran <93713092+Xernaxe@users.noreply.github.com>
2023-10-11 16:56:33 +0300

    transformed hero section from tailwind to scss

--- src/app/components/Hero/Hero.tsx
+++ src/app/components/Hero/Hero.tsx
@@ -3,19 +3,20 @@ import React from 'react';
 import { SkewedButton } from '../_globals/SkewedButton';
 import Image from 'next/image';
 import { HeroImg } from './HeroImg';
+import styles from './Hero.module.scss'
 
 export const Hero = () => {
 	return (
 		<>
-			<section className='hero | flex flex-col gap-[3.125rem] relative mb-20 tablet:mb-28 desktop:mb-40 pb-10 w-full tablet:flex-row tablet:justify-center tablet:py-8 tablet:items-center tablet:gap-9 desktop:gap-[4.83rem] desktop:pb-0 desktopL:gap-[3.64rem]'>
-				<div className='mt-[3.125rem] z-20 tablet:mt-0'>
-					<h1 className='font-oxanium text-2xl font-semibold [word-spacing:0.5rem] leading-loose  tablet:text-[clamp(1.75rem,3.4vw,2.1rem)] tablet:[word-spacing:normal] desktop:text-[clamp(1.75rem,3.4vw,3rem)]'>
+			<section className={styles.heroSection}>
+				<div className={styles.heroTextWrapper}>
+					<h1>
 						Welcome to the{' '}
-						<span className=' text-greenCyan font-[inherit] text-2xl font-semibold tablet:text-[clamp(1.75rem,3.4vw,2.1rem)] desktop:text-[clamp(1.75rem,3.4vw,3rem)]'>
+						<span>
 							Esports
 						</span>{' '}
 					</h1>
-					<h2 className='font-bold text-[2rem] font-[oxanium] leading-none mb-6 tablet:text-5xl tablet:font-semibold desktop:text-[clamp(3rem,4.5vw,4rem)] desktop:mb-10'>
+					<h2>
 						Multiverse
 					</h2>
 
@@ -27,42 +28,42 @@ export const Hero = () => {
 				</div>
 
 				{/* @TODO: CHANGE PX TO RESPONSIVE */}
-				<div className='flex justify-center flex-wrap gap-x-6 z-20 desktop:mt-[3%] desktop:h-[573px] desktop:overflow-hidden desktop:gap-x-7'>
-					<div className='flex flex-col gap-y-10 desktopL:gap-y-[3.5625rem]'>
-						<Image
-							className='object-cover w-[clamp(9.6875rem,_10vw,_15rem)] tablet:w-[clamp(9.6875rem,_20.1vw,_16.875rem)] tablet:h-[clamp(10.5rem,_21vw,_18.375rem)] h-[10.5rem] rounded desktop:rounded-[0.625rem]'
-							width={155}
-							height={168}
-							src='/images/cs-go.png'
-							alt='CS:GO'
-							priority={true}
-						/>
-						<Image
-							className='object-cover w-[clamp(9.6875rem,_10vw,_15rem)] tablet:w-[clamp(9.6875rem,_20.1vw,_16.875rem)] tablet:h-[clamp(10.5rem,_21vw,_18.375rem)] h-[10.5rem] rounded desktop:rounded-[0.625rem]'
-							width={155}
-							height={168}
-							src='/images/fortnite.png'
-							alt='Fortnite'
-							priority={true}
-						/>
+				<div className={styles.imagesContainer}>
+					<div>
+						<div>
+							<Image
+								fill
+								src='/images/cs-go.png'
+								alt='CS:GO'
+								priority={true}
+							/>
+						</div>
+						<div>
+							<Image
+								fill
+								src='/images/fortnite.png'
+								alt='Fortnite'
+								priority={true}
+							/>
+						</div>
 					</div>
-					<div className='flex flex-col gap-y-10 mt-[0.875rem] desktop:mt-[1.6875rem] desktopL:gap-y-[3.5625rem]'>
-						<Image
-							className='object-cover w-[clamp(9.6875rem,_10vw,_15rem)] tablet:w-[clamp(9.6875rem,_20.1vw,_16.875rem)] tablet:h-[clamp(10.5rem,_21vw,_18.375rem)] h-[10.5rem] rounded desktop:rounded-[0.625rem]'
-							width={155}
-							height={168}
-							src='/images/pubg.png'
-							alt='PUBG'
-							priority={true}
-						/>
-						<Image
-							className='object-cover w-[clamp(9.6875rem,_10vw,_15rem)] tablet:w-[clamp(9.6875rem,_20.1vw,_16.875rem)] tablet:h-[clamp(10.5rem,_21vw,_18.375rem)] h-[10.5rem] rounded desktop:rounded-[0.625rem]'
-							width={155}
-							height={168}
-							src='/images/valorant.png'
-							alt='Valorant'
-							priority={true}
-						/>
+					<div>
+						<div>
+							<Image
+								fill
+								src='/images/pubg.png'
+								alt='PUBG'
+								priority={true}
+							/>
+						</div>
+						<div>
+							<Image
+								fill
+								src='/images/valorant.png'
+								alt='Valorant'
+								priority={true}
+							/>
+						</div>
 					</div>
 				</div>
 				<HeroImg />
