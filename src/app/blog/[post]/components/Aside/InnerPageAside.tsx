e5466aa2ee6a6f077709df0f7024d1aab0c2dba0
Mihai-Alexandru Vistran <93713092+Xernaxe@users.noreply.github.com>
2024-02-20 23:56:55 +0200

    Update InnerPageAside.tsx

--- src/app/blog/[post]/components/Aside/InnerPageAside.tsx
+++ src/app/blog/[post]/components/Aside/InnerPageAside.tsx
@@ -2,18 +2,19 @@ import React from 'react';
 import { AsideSection } from './AsideSection';
 import { AsideSectionGames } from './AsideSectionGames';
 import { AsideSectionTournaments } from './AsideSectionTournaments';
+import { AsideSectionDeal } from './AsideSectionDeal';
 
 export const InnerPageAside = () => {
 	return (
 		<aside className='hidden flex-col gap-10  desktopL:flex'>
 			<AsideSection title={'Upcoming tournaments'}>
-				<AsideSectionTournaments className='border-b-4 border-b-postAccent' />
+				<AsideSectionTournaments className='border-b-4 pt-0 border-b-postAccent' />
 				<AsideSectionTournaments className='border-b-4 border-b-postAccent' />
 				<AsideSectionTournaments className='rouned-b-[10px]' />
 			</AsideSection>
 
 			<AsideSection title={'Our Upcoming Games'}>
-				<AsideSectionGames className='border-b-4 border-b-postAccent' />
+				<AsideSectionGames className='border-b-4 pt-0 border-b-postAccent' />
 				<AsideSectionGames className='border-b-4 border-b-postAccent' />
 				<AsideSectionGames className='rounded-b-[10px]' />
 			</AsideSection>
