9213793b9440ba68c9fb87c4590e1f5dbb429ffe
Xernaxe <93713092+Xernaxe@users.noreply.github.com>
2024-10-05 16:39:59 +0300

    upgraded packages to latest, added copylink functionality

--- src/app/components/_globals/ShareItem/ShareButton.tsx
+++ src/app/components/_globals/ShareItem/ShareButton.tsx
@@ -1,9 +1,17 @@
+'use client';
+
 import React from 'react';
 import Image from 'next/image';
 
 export const ShareButton = () => {
+	const onClickHandler = () => {
+		navigator.clipboard.writeText(window.location.href);
+	};
 	return (
-		<button className='flex items-center gap-5 border border-postAccent p-2 tablet:py-3 tablet:px-4 tablet:border-[2px]'>
+		<button
+			onClick={onClickHandler}
+			className='flex items-center gap-5 border border-postAccent p-2 tablet:py-3 tablet:px-4 tablet:border-[2px]'
+		>
 			<span className='font-oxanium font-bold uppercase'>Copy Link</span>
 			<Image src={'/images/copy.svg'} alt='Copy' width={25} height={25} />
 		</button>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,6 @@ import { AboutUs } from './components/AboutUs/AboutUs';
 import { Hero } from './components/Hero/Hero';
 import { LatestNews } from './components/LatestNews/LatestNews';
 import { OurShop } from './components/OurShop/OurShop';
-import { TournamentBanner } from './components/Banner/TournamentBanner';
 import { Tournaments } from './components/Tournaments/Tournaments';
 import { Contact } from './components/Contact/Contact';
 
